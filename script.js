31e5cb316789013a017a87d3283fe26b99c16688
Kyle Thomas <kylejwthomas@gmail.com>
2022-08-04 14:32:24 +0900

    added backgrounds folder and images

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
-playerImage.src = 'shadow_dog.png';
+playerImage.src = './sprites/shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
 
